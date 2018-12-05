926880a5d79aab12a75b8df74bbcc4ab053b0b43
Romilson Alves <vic_alves@hotmail.fr>
2018-12-05 12:30:48 +0100

    ok

--- Force/DBDoc/DependencyInjection/ForceDBDocExtension.php
+++ Force/DBDoc/DependencyInjection/ForceDBDocExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ForceDBdocExtension extends Extension
+class ForceDBDocExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
