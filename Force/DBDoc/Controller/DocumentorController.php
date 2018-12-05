490efbdc81cf7a97032b852e360839448417da1d
Romilson Alves <vic_alves@hotmail.fr>
2018-12-05 15:11:42 +0100

    ok

--- Force/DBDoc/Controller/DocumentorController.php
+++ Force/DBDoc/Controller/DocumentorController.php
@@ -1,11 +1,20 @@
 <?php
 namespace Force\DBDoc\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class DocumentorController
 {
+    private $content;
+    
     public function allAction()
     {
-        return 'Hello world';
+        return new Response('Hello world');
+    }
+    
+    public function setContent(string $content) {
+        $this->content = $content;
+        return $this;
     }
 }
 
